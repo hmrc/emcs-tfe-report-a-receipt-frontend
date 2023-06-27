7e08767e0cb4856fa75b6d9238aac2d8c9b021cf
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2023-06-27 15:18:37 +0100

    [ETFE-1260] Rebased with latest ETFE-1248

--- test/controllers/ItemMoreInformationControllerSpec.scala
+++ test/controllers/ItemMoreInformationControllerSpec.scala
@@ -160,7 +160,7 @@ class ItemMoreInformationControllerSpec extends SpecBase
           val result = route(application, request).value
 
           status(result) mustEqual SEE_OTHER
-          redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+          redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad(testErn, testArc).url
         }
 
         "must redirect to Journey Recovery for a POST if no existing data is found" in new Fixture(None) {
@@ -170,7 +170,7 @@ class ItemMoreInformationControllerSpec extends SpecBase
           val result = route(application, request).value
 
           status(result) mustEqual SEE_OTHER
-          redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+          redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad(testErn, testArc).url
         }
       }
     }

61186e8e296be6ef6c6657e60126f8ed16a396ff
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2023-07-05 09:56:58 +0100

    [ETFE-1332c] Update RefusedAmountController to use the ReferenceDataService

--- test/controllers/RefusedAmountControllerSpec.scala
+++ test/controllers/RefusedAmountControllerSpec.scala
@@ -18,7 +18,7 @@ package controllers
 
 import base.SpecBase
 import forms.RefusedAmountFormProvider
-import mocks.services.{MockGetCnCodeInformationService, MockUserAnswersService}
+import mocks.services.{MockGetCnCodeInformationService, MockReferenceDataService, MockUserAnswersService}
 import models.UnitOfMeasure.Kilograms
 import models.{NormalMode, UserAnswers}
 import navigation.{FakeNavigator, Navigator}
@@ -27,19 +27,19 @@ import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import services.{GetCnCodeInformationService, UserAnswersService}
+import services.{GetCnCodeInformationService, ReferenceDataService, UserAnswersService}
 import views.html.RefusedAmountView
 
 import scala.concurrent.Future
 
-class RefusedAmountControllerSpec extends SpecBase with MockUserAnswersService with MockGetCnCodeInformationService {
+class RefusedAmountControllerSpec extends SpecBase with MockUserAnswersService with MockReferenceDataService {
 
   class Fixture(val userAnswers: Option[UserAnswers] = Some(emptyUserAnswers)) {
     val application = applicationBuilder(userAnswers)
       .overrides(
         bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
         bind[UserAnswersService].toInstance(mockUserAnswersService),
-        bind[GetCnCodeInformationService].toInstance(mockGetCnCodeInformationService)
+        bind[ReferenceDataService].toInstance(mockReferenceDataService)
       )
       .build()
 
@@ -63,15 +63,15 @@ class RefusedAmountControllerSpec extends SpecBase with MockUserAnswersService w
     "must return OK and the correct view for a GET" in new Fixture(Some(emptyUserAnswers.set(SelectItemsPage(1), 1))) {
       running(application) {
 
-        MockGetCnCodeInformationService.getCnCodeInformationWithMovementItems(Seq(item1)).returns(Future.successful(Seq(
-          (item1, cnCodeInfo)
-        )))
+        MockReferenceDataService.itemWithReferenceData(item1).onCall(
+          MockReferenceDataService.itemWithReferenceDataSuccessHandler(item1WithReferenceData, cnCodeInfo)
+        )
 
         val request = FakeRequest(GET, refusedAmountRoute)
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, refusedAmountSubmitAction, Kilograms, item1, cnCodeInfo, 1)(dataRequest(request), messages(application)).toString
+        contentAsString(result) mustEqual view(form, refusedAmountSubmitAction, Kilograms, item1WithReferenceData, cnCodeInfo, 1)(dataRequest(request), messages(application)).toString
       }
     }
 
@@ -82,15 +82,15 @@ class RefusedAmountControllerSpec extends SpecBase with MockUserAnswersService w
     )) {
       running(application) {
 
-        MockGetCnCodeInformationService.getCnCodeInformationWithMovementItems(Seq(item1)).returns(Future.successful(Seq(
-          (item1, cnCodeInfo)
-        )))
+        MockReferenceDataService.itemWithReferenceData(item1).onCall(
+          MockReferenceDataService.itemWithReferenceDataSuccessHandler(item1WithReferenceData, cnCodeInfo)
+        )
 
         val request = FakeRequest(GET, refusedAmountRoute)
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(validAnswer), refusedAmountSubmitAction, Kilograms, item1, cnCodeInfo, 1)(dataRequest(request), messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(validAnswer), refusedAmountSubmitAction, Kilograms, item1WithReferenceData, cnCodeInfo, 1)(dataRequest(request), messages(application)).toString
       }
     }
 
@@ -113,9 +113,9 @@ class RefusedAmountControllerSpec extends SpecBase with MockUserAnswersService w
     "must return a Bad Request and errors when invalid data is submitted" in new Fixture(Some(emptyUserAnswers.set(SelectItemsPage(1), 1))) {
       running(application) {
 
-        MockGetCnCodeInformationService.getCnCodeInformationWithMovementItems(Seq(item1)).returns(Future.successful(Seq(
-          (item1, cnCodeInfo)
-        )))
+        MockReferenceDataService.itemWithReferenceData(item1).onCall(
+          MockReferenceDataService.itemWithReferenceDataSuccessHandler(item1WithReferenceData, cnCodeInfo)
+        )
 
         val request =
           FakeRequest(POST, refusedAmountRoute)
@@ -125,7 +125,7 @@ class RefusedAmountControllerSpec extends SpecBase with MockUserAnswersService w
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, refusedAmountSubmitAction, Kilograms, item1, cnCodeInfo, 1)(dataRequest(request), messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, refusedAmountSubmitAction, Kilograms, item1WithReferenceData, cnCodeInfo, 1)(dataRequest(request), messages(application)).toString
       }
     }
 

bbf9867f8ede9c028a636cdaa1fd487c4164fff1
james-work-account <31282758+james-work-account@users.noreply.github.com>
2023-11-13 18:05:04 +0000

    [ETFE-typo] LocalDate -> LocalDateTime on CYA confirmation

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -38,7 +38,7 @@ import uk.gov.hmrc.http.HeaderCarrier
 import viewmodels.checkAnswers.{CheckAnswersHelper, CheckAnswersItemHelper}
 import views.html.CheckYourAnswersView
 
-import java.time.LocalDate
+import java.time.LocalDateTime
 import javax.inject.Inject
 import scala.concurrent.Future
 
@@ -147,7 +147,7 @@ class CheckYourAnswersController @Inject()(override val messagesApi: MessagesApi
         data = Json.obj(ConfirmationPage.toString ->
           ConfirmationDetails(
             receiptStatus = request.userAnswers.get(AcceptMovementPage).getOrElse().toString,
-            receiptDate = LocalDate.now().toString,
+            receiptDate = LocalDateTime.now().toString,
             hasMovementShortage = request.userAnswers.get(WrongWithMovementPage).exists(_.contains(Shortage)),
             hasItemShortage = request.userAnswers.completedItems.exists(_.itemShortageOrExcess.exists(_.wrongWithItem == Shortage)),
             hasMovementExcess = request.userAnswers.get(WrongWithMovementPage).exists(_.contains(Excess)),

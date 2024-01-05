d25eb4e0d3e556f8cebfacbd0e8c9836cd1394c6
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-01-05 14:31:29 +0000

    [ETFE-2992] update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,26 +2,26 @@ import sbt.*
 
 object AppDependencies {
 
-  val playSuffix = "-play-28"
+  val playSuffix = "-play-30"
   val scalatestVersion = "3.2.15"
-  val hmrcBootstrapVersion = "7.22.0"
-  val hmrcMongoVersion = "1.3.0"
+  val hmrcBootstrapVersion = "8.4.0"
+  val hmrcMongoVersion = "1.6.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %%  "play-frontend-hmrc"                % s"7.21.0$playSuffix",
-    "uk.gov.hmrc"             %% s"bootstrap-frontend$playSuffix"     %  hmrcBootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo$playSuffix"             %  hmrcMongoVersion,
-    "com.google.inject"       %   "guice"                             % "5.1.0"
+    "uk.gov.hmrc"             %%  s"play-frontend-hmrc$playSuffix" % "8.3.0",
+    "uk.gov.hmrc"             %% s"bootstrap-frontend$playSuffix"  %  hmrcBootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo$playSuffix"          %  hmrcMongoVersion,
+    "com.google.inject"       %   "guice"                          % "5.1.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"         % hmrcBootstrapVersion,
-    "org.scalatestplus"       %%  "scalacheck-1-17"                   % s"$scalatestVersion.0",
-    "org.scalatestplus.play"  %%  "scalatestplus-play"                % "5.1.0",
-    "org.scalamock"           %%  "scalamock"                         % "5.2.0",
-    "org.jsoup"               %   "jsoup"                             % "1.15.4",
-    "com.vladsch.flexmark"    %   "flexmark-all"                      % "0.62.2"
+    "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix" % hmrcBootstrapVersion,
+    "org.scalatestplus"       %%  "scalacheck-1-17"           % s"$scalatestVersion.0",
+    "org.scalatestplus.play"  %%  "scalatestplus-play"        % "5.1.0",
+    "org.scalamock"           %%  "scalamock"                 % "5.2.0",
+    "org.jsoup"               %   "jsoup"                     % "1.15.4",
+    "com.vladsch.flexmark"    %   "flexmark-all"              % "0.62.2"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test

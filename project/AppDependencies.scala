56c089194232c2ccb633a56905d4f74c6563552e
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2024-02-22 10:02:14 +0000

    [ETFE-3468] play-frontend-hmrc upgrade (#196)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ object AppDependencies {
   val playSuffix = "-play-30"
   val scalatestVersion = "3.2.15"
   val hmrcBootstrapVersion = "8.4.0"
-  val hmrcMongoVersion = "1.6.0"
+  val hmrcMongoVersion = "1.7.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %%  s"play-frontend-hmrc$playSuffix" % "8.3.0",
+    "uk.gov.hmrc"             %%  s"play-frontend-hmrc$playSuffix" % "8.5.0",
     "uk.gov.hmrc"             %% s"bootstrap-frontend$playSuffix"  %  hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo$playSuffix"          %  hmrcMongoVersion,
     "com.google.inject"       %   "guice"                          % "5.1.0"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,8 +4,8 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 
 resolvers += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.18.0")
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.4.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.20.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.5.0")
 
 addSbtPlugin("org.playframework" % "sbt-plugin" % "3.0.1")
 
